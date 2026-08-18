44173f8460c0caaf8d0e32ed2c1e2b3ac499e7a2
Ian <isturgill@gmail.com>
2026-08-17 21:21:01 -0400

    Add script for running samtools mpileup and VarScan2

---
+++ data/02-run_varscan.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# This script first runs samtools mpileup individually on normal and tumor BAMs.
+# The mpileups are then used with VarScan2 for somatic variant calling. 
+# Note: FASTA and BAM files should have associated index files in the same directory
+DOCKER="mgibio/varscan-cwl:v2.4.2-samtools1.16.1"
+REF_FASTA="ref/GRCh38.d1.vd1.fa"
+NORMAL_BAM="data/WES_EA_N_1.bwa.dedup.bam"
+TUMOR_BAM="data/WES_EA_T_1.bwa.dedup.bam"
+SAMPLE=$(basename ${NORMAL_BAM} _N_1.bwa.dedup.bam)
+NORMAL_PILEUP="data/varscan2/${SAMPLE}.normal.pileup"
+TUMOR_PILEUP="data/varscan2/${SAMPLE}.tumor.pileup"
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+set -euo pipefail
+mkdir -p varscan2
+
+docker run --rm --user $(id -u):$(id -g) -v ..:/data -w /data ${DOCKER} \
+	bash -c '
+		# Generate mpileup files for both normal and tumor BAMs
+		samtools mpileup -f '"${REF_FASTA}"' -q 1 -B '"${NORMAL_BAM}"' > '"${NORMAL_PILEUP}"' & \
+		normal_pid=$!
+		samtools mpileup -f '"${REF_FASTA}"' -q 1 -B '"${TUMOR_BAM}"' > '"${TUMOR_PILEUP}"' & \
+		tumor_pid=$!
+		
+		# Wait for both processes to finish before proceeding to VarScan2
+		wait $normal_pid
+		wait $tumor_pid
+
+		# Run VarScan2 and output as VCF
+		java -jar /opt/varscan/VarScan.jar somatic \
+			'"${NORMAL_PILEUP}"' \ 
+			'"${TUMOR_PILEUP}"' \
+			data/varscan2/'"${SAMPLE}"' \
+			--output-vcf
+	'
